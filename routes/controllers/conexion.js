86edb23b220fd12364ce95e76f95fc142eaad03f
Juan Jesus Mata Solis <jujemataso@ittepic.edu.mx>
2022-11-09 22:04:35 -0700

    AJUSTES DE CONEXION

--- routes/controllers/conexion.js
+++ routes/controllers/conexion.js
@@ -2,12 +2,11 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 
 const mongo = () => {
-  return mongoose.connect('mongodb://localhost:27017/gestionEscolar');
+  return mongoose.connect('mongodb+srv://mata9909:TitanFold13258@cluster0.fkeew43.mongodb.net/gestionEscolar?retryWrites=true&w=majority');
 };
 
 const conRedis = () => {
   const client = redis.createClient();
   return client;
 };
-
 module.exports = { mongo, redis: conRedis };
